a96b16c6eefa966d633665b8770db61739bdb23c
zrouviere <zrouviere@gmail.com>
2019-11-11 14:12:17 -0500

    created config file for client module

---
+++ client/src/components/Firebase/firebase.js
@@ -0,0 +1,31 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+var rawConfig = require('../../config/config');
+
+const config = {
+    apiKey: rawConfig.firebase.REACT_APP_API_KEY,
+    authDomain: rawConfig.firebase.REACT_APP_AUTH_DOMAIN,
+    databaseURL: rawConfig.firebase.REACT_APP_DATABASE_URL,
+    projectId: rawConfig.firebase.REACT_APP_PROJECT_ID,
+    storageBucket: rawConfig.firebase.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: rawConfig.firebase.REACT_APP_MESSAGING_SENDER_ID,
+    appId: rawConfig.firebase.REACT_APP_APP_ID
+};
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+        this.auth = app.auth();
+    }
+    // *** Auth API ***
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+    doSignOut = () => this.auth.signOut();
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
+}
+export default Firebase;
