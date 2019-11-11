4ad3c8903f6b75007ddadbce168e2fc761707494
Thomas Mulligan <thomas.mulligan@ufl.edu>
2019-11-11 16:13:50 -0500

    Something for header

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => (
+    <div>
+        <h1>This is a Header</h1>
+    </div>
+);
+export default Header;
