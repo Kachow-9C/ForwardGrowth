a51e09b31c8e5a984e2e0f5e8503cc98c587d95a
zrouviere <zrouviere@gmail.com>
2019-11-11 12:16:09 -0500

    Finished Sign Out and Session Handling. Move Anushris sign in to .old for now but will reintegrate later

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,42 +1,72 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
-import SignInForm from './SignInForm';
-import './SignIn.css';
-
-function SignIn() {
-    return (
-        <div className = "App">
-            <div className = "App__Aside"></div>
-            <div className = "App__Form">
-                <div className="PageSwitcher">
-                    <a href = "#" className = "PageSwitcher__Item PageSwitcher__Item--Active">Sign In</a>
-                </div>
-
-                <div className="FormTitle">
-                    <a href = "#" className= "FormTitle__Link"> Sign In</a>
-                </div>
-
-                <div className="FormCenter">
-                    <form className="FormFields">
-                        <div className="FormField">
-                            <label className="FormField_Label" htmlFor="name">Username</label>
-                            <div><input type = "text" id="name" className="FormField__Input" placeHolder = "Enter username" name ="name"></input></div>
-                        </div>
-                        <div className="FormField">
-                            <label className="FormField_Label" htmlFor="name">Password</label>
-                            <div><input type = "password" id="name" className="FormField__Input" placeHolder = "Enter Password"></input></div>
-                        </div>
-
-                        <div className = "FormField">
-                            <button className = "FormField__Button mr-20"> Sign In</button>
-                        </div>
-                    </form>
-                </div>
-
-
-            </div>
-        </div>
-    );
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { SignUpLink } from '../SignUp';
+import { withFirebase } from '../Firebase';
+import * as ROUTES from '../../constants/routes';
+const SignInPage = () => (
+    <div>
+        <h1>SignIn</h1>
+        <SignInForm />
+        <SignUpLink />
+    </div>
+);
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null,
+};
+class SignInFormBase extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { ...INITIAL_STATE };
+    }
+    onSubmit = event => {
+        const { email, password } = this.state;
+        this.props.firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
+            })
+            .catch(error => {
+                this.setState({ error });
+            });
+        event.preventDefault();
+    };
+    onChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+    render() {
+        const { email, password, error } = this.state;
+        const isInvalid = password === '' || email === '';
+        return (
+            <form onSubmit={this.onSubmit}>
+                <input
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    placeholder="Email Address"
+                />
+                <input
+                    name="password"
+                    value={password}
+                    onChange={this.onChange}
+                    type="password"
+                    placeholder="Password"
+                />
+                <button disabled={isInvalid} type="submit">
+                    Sign In
+                </button>
+                {error && <p>{error.message}</p>}
+            </form>
+        );
+    }
 }
-export default SignIn;
+const SignInForm = compose(
+    withRouter,
+    withFirebase,
+)(SignInFormBase);
+export default SignInPage;
+export { SignInForm };
