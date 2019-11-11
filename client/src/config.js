46dad813dda10fc10d540dd5627882e3ce9b752d
anushrimarar <anushri.marar@ufl.edu>
2019-11-11 15:46:33 -0500

    css added to the sign in page (2)

---
+++ client/src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+    },
+    firebase: {
+        REACT_APP_API_KEY: "AIzaSyBJkh6JwI-Bv6HiGjIrNP4bgazlUEeAt3c",
+        REACT_APP_AUTH_DOMAIN: "forward-growth.firebaseapp.com",
+        REACT_APP_DATABASE_URL: "https://forward-growth.firebaseio.com",
+        REACT_APP_PROJECT_ID: "forward-growth",
+        REACT_APP_STORAGE_BUCKET: "forward-growth.appspot.com",
+        REACT_APP_MESSAGING_SENDER_ID: "932636624978",
+        REACT_APP_APP_ID: "1:932636624978:web:cabc3d324030d437da378b",
+    }
+};
