1eacef75e2e5a8d6e03b55236bc1554f3dbf7314
Vinay Reddy <VinayR@Vinays-MacBook-Pro.local>
2019-11-11 14:06:27 -0500

    altered config file staged for deployment

--- server/config/config.js
+++ server/config/config.js
@@ -6,6 +6,6 @@
 
 module.exports = {
     db: {
-        uri: 'mongodb+srv://server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+        uri:'none', //place the URI of your mongo database here.
     }
 };
