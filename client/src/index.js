a5714bd3b04137c97e00bf05ea62a727197ac991
zrouviere <zrouviere@gmail.com>
2019-11-11 11:59:53 -0500

    Sign up firebase components complete

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 
 ReactDOM.render(
     <Router>
+        <FirebaseContext.Provider value={new Firebase()}>
+
         <App />
+        </FirebaseContext.Provider>,
+
     </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
