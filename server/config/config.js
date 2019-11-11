afc12bbf5d8029b23aaab189949aafe249f637b6
Vinay Reddy <VinayR@Vinays-MacBook-Pro.local>
2019-11-11 12:10:49 -0500

    added deployment branch for heroku

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+//This file holds any configuration variables we may need
+//'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
+
+//server
+//57KZYtQ93glUyECs
+
+module.exports = {
+    db: {
+        uri: 'mongodb+srv://server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+    }
+};
