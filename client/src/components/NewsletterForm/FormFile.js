317f643196e94124897b1885fcf71bd0921e27c9
Thomas Mulligan <thomas.mulligan@ufl.edu>
2019-11-22 14:53:46 -0500

    Base Newsletter form created

---
+++ client/src/components/NewsletterForm/FormFile.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class FormFile extends Component {
+    constructor() {
+        super();
+        this.state = { newsletters: [] }
+    }
+
+    addNews() {
+        var listing = {
+            title: this.title.value,
+            content: this.title.value
+        }
+    }
+
+    render() {
+        return (
+            <form>
+                <label>Enter News Here:</label>
+                <br></br>
+                <br></br>
+                <label>
+                    Title:
+                    <input type="text" placeholder="Enter Title" ref={(title) => this.title = title} />
+                </label>
+                <label>
+                    Content:
+                    <input type="text" placeholder="Enter Content" ref={(content) => this.content = content} />
+                </label>
+                <input className="Button" type="button" value="Add News" onClick={this.addNews.bind(this)}/>
+            </form>
+        );
+    }
+}
+export default FormFile;
