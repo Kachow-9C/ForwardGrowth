470fcfec105bcb54a4a9e272073fdf4380edf9f7
zrouviere <zrouviere@gmail.com>
2019-11-21 21:25:12 -0500

    Restricted backend for authenticated users only. Made slight textual modifications to footer.

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,8 @@ import * as ROUTES from "../../constants/routes";
 
 const Footer = () => (
     <div style={{color: 'white', textAlign: 'center'}}>
-        Copyright Kachow! Development Team | <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        <br></br>
+        Copyright 2019 Forward Growth | Are you a team member? <Link to={ROUTES.SIGN_IN}>Sign In Here</Link>
 
     </div>
 );
