f1a9403770ffa28e83aa5b9adacf7e9f226452a0
Vinay Reddy <VinayR@Vinays-MacBook-Pro.local>
2019-11-22 17:53:54 -0500

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('./config/express.js')
+const express_2 = require('express')
 const path = require('path')
 // Use env port or default
 const port = process.env.PORT || 5000;
@@ -6,7 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express.init()
 
 if (process.env.NODE_ENV==='production') {
-    app.use(express.static('client/build'));
+    app.use(express_2.static('client/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 
