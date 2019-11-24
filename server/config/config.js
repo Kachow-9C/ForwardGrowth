676a61192653886552612cda6f2e7e23d1e042f8
Vinay Reddy <VinayR@Vinays-MacBook-Pro.local>
2019-11-23 21:19:57 -0500

    edited config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     db: {
-        uri: process.env.MONGO_URI, //place the URI of your mongo database here.
+        uri: 'mongodb+srv://server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority'//server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority, //place the URI of your mongo database here.
     },
 };
