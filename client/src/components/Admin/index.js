6ae1b9034b3a4346174db66057499c12d0b9e5db
zrouviere <zrouviere@gmail.com>
2019-11-25 14:27:09 -0500

    Moved sign up component to backend. Secured it so only logged in users can register a new user. Removed "forgot password"  from admin console as it is redundant with the "change password" functionality.

--- client/src/components/Admin/index.js
+++ client/src/components/Admin/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 import { AuthUserContext, withAuthorization } from '../Session';
 import FormFile from '../NewsPost/FormFile.js';
 import Container from 'react-bootstrap/Container'
+import {Link} from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+
 
 const AdminPage = () => (
     <AuthUserContext.Consumer>
@@ -13,8 +15,12 @@ const AdminPage = () => (
                 <Container>
                     <h3 className='text-center' style={{color:'#fff'}}> Welcome: {authUser.email} </h3>
                 </Container><br/><br/><br/><br/>
-                <PasswordForgetForm /><br/><br/><br/><br/>
-                <PasswordChangeForm /><br/><br/><br/><br/>
+                <PasswordChangeForm /><br/>
+                <Container>
+                    <h3 className='text-center' style={{color:'#fff'}}>Want to add another team member? <Link to={ROUTES.SIGN_UP}>Create a New Account</Link></h3>
+                </Container>
+                <br/><br/><br/>
+
                 <FormFile/>
             </div>
 
--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-
 import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
+import {AuthUserContext, withAuthorization} from "../Session";
 
 
 const SignUpPage = () => (
@@ -59,52 +59,53 @@ class SignUpFormBase extends Component {
             username === '';
 
         return (
-            <form onSubmit={this.onSubmit}>
-                <input
-                    name="username"
-                    value={username}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Full Name"
-                />
-                <input
-                    name="email"
-                    value={email}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <input
-                    name="passwordOne"
-                    value={passwordOne}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Password"
-                />
-                <input
-                    name="passwordTwo"
-                    value={passwordTwo}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Confirm Password"
-                />
-                <button disabled={isInvalid} type="submit">
-                    Sign Up
-                </button>
-                {error && <p>{error.message}</p>}
-            </form>
+            <AuthUserContext.Consumer>
+                {authUser => (
+                    <form onSubmit={this.onSubmit}>
+                        <input
+                            name="username"
+                            value={username}
+                            onChange={this.onChange}
+                            type="text"
+                            placeholder="Full Name"
+                        />
+                        <input
+                            name="email"
+                            value={email}
+                            onChange={this.onChange}
+                            type="text"
+                            placeholder="Email Address"
+                        />
+                        <input
+                            name="passwordOne"
+                            value={passwordOne}
+                            onChange={this.onChange}
+                            type="password"
+                            placeholder="Password"
+                        />
+                        <input
+                            name="passwordTwo"
+                            value={passwordTwo}
+                            onChange={this.onChange}
+                            type="password"
+                            placeholder="Confirm Password"
+                        />
+                        <button disabled={isInvalid} type="submit">
+                            Sign Up
+                        </button>
+                        {error && <p>{error.message}</p>}
+                    </form>
+                )}
+
+            </AuthUserContext.Consumer>
+
         );
     }
 }
-const SignUpLink = () => (
-    <p>
-        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-    </p>
-);
+
 const SignUpForm = compose(
     withRouter,
     withFirebase,
 )(SignUpFormBase);
-
-export default SignUpPage;
-export { SignUpForm, SignUpLink };
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(SignUpPage);
