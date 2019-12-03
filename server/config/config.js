07ce7ed23e1b8535cfd65badd7175faf275086d1
Vinay Reddy <VinayR@Vinays-MacBook-Pro.local>
2019-12-02 19:24:21 -0500

    fixes config files

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     db: {
-        uri: 'mongodb+srv://server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority'//server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority, //place the URI of your mongo database here.
+        uri: process.env.MONGO_URI_1 + process.env.MONGO_URI_2 + process.env.MONGO_URI_3 + process.env.MONGO_URI_4
+        //server:57KZYtQ93glUyECs@cluster0-kmygl.mongodb.net/test?retryWrites=true&w=majority, //place the URI of your mongo database here.
     },
 };
